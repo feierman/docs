d75859f1ce8251d04c3ce772ed27d1c9a419c262
feierman <1981879111@qq.com>
2024-11-30 23:40:03 +0800

    Update documentation and examples for HTML5 Canvas drawing techniques, including new sections on gradients, paths, shapes, transformations, and image handling. Also, clean up package dependencies and remove unused files.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -22,6 +22,10 @@ export default defineConfig({
                 {
                     text: 'HTML5指南',
                     link: '/docs-notes/frontend/html/1.menu.md',
+                    collapsed: true,
+                    items: [
+                        
+                    ]
                 },
                 {
                     text: 'HTML基础概念',
@@ -34,25 +38,41 @@ export default defineConfig({
                     text: '基础篇',
                     collapsed: true,
                     items: [
-                        { text: "HTML5文档结构结构", link: "/docs-notes/frontend/html/Basis/1.structure/1.1.md" },
-                        { text: "语义化标签", link: "/docs-notes/frontend/html/Basis/1.structure/1.2.md" },
-                        { text: "页面布局基础", link: "/docs-notes/frontend/html/Basis/1.structure/1.3.md" },
-                        { text: "文本格式化", link: "/docs-notes/frontend/html/Basis/2.test/1.1.md" },
-                        {text:  "序列标签",link:"/docs-notes/frontend/html/Basis/2.test/1.4.md"},
-                        { text: "链接和图片", link: "/docs-notes/frontend/html/Basis/2.test/1.2.md" },
-                        { text: "表格应用", link: "/docs-notes/frontend/html/Basis/2.test/1.3.md" },
+                        { text: "1 HTML5文档结构结构", link: "/docs-notes/frontend/html/Basis/1.structure/1.1.md" },
+                        { text: "2 语义化标签", link: "/docs-notes/frontend/html/Basis/1.structure/1.2.md" },
+                        { text: "3 页面布局基础", link: "/docs-notes/frontend/html/Basis/1.structure/1.3.md" },
+                        { text: "4 文本格式化", link: "/docs-notes/frontend/html/Basis/2.test/1.1.md" },
+                        {text:  "5 序列标签",link:"/docs-notes/frontend/html/Basis/2.test/1.4.md"},
+                        { text: "6 链接和图片", link: "/docs-notes/frontend/html/Basis/2.test/1.2.md" },
+                        { text: "7 表格应用", link: "/docs-notes/frontend/html/Basis/2.test/1.3.md" },
                         { text: "表单基础", link: "/docs-notes/frontend/html/Basis/3.form/1.1.md" },
-                        { text: "新增表单元素", link: "/docs-notes/frontend/html/Basis/3.form/1.2.md" },
-                        { text: "表单验证", link: "/docs-notes/frontend/html/Basis/3.form/1.3.md" }
+                        { text: "8 新增表单元素", link: "/docs-notes/frontend/html/Basis/3.form/1.2.md" },
+                        { text: "9 表单验证", link: "/docs-notes/frontend/html/Basis/3.form/1.3.md" }
                     ]
                 },
-                
+                {
+                    text: "进阶",
+                    collapsed: true,
+                    items: [
+                        { text: "1 Canvas绘图", link: "/docs-notes/frontend/html/Advanced/draw/1.1.md" },
+                        { text: "2 Canvas渐变", link: "/docs-notes/frontend/html/Advanced/draw/1.2.md" },
+                        { text: "3 Canvas路径", link: "/docs-notes/frontend/html/Advanced/draw/1.3.md" },
+                        { text: "4 Canvas绘制圆形", link: "/docs-notes/frontend/html/Advanced/draw/1.5.md" },
+                        { text: "5 Canvas变换", link: "/docs-notes/frontend/html/Advanced/draw/1.6.md" },
+                        { text: "6 高级绘图", link: "/docs-notes/frontend/html/Advanced/draw/1.7.md" },
+                        { text: "7 图形阴影", link: "/docs-notes/frontend/html/Advanced/draw/1.8.md" },
+                        { text: "8 绘制图像", link: "/docs-notes/frontend/html/Advanced/drawImage/1.1.md" }
+
+                    ]
+                }
             ]
         },
         docFooter: {
-            prev: '上一页',
+            prev: '上一页',  
             next: '下一页'
         },
-    },
-
+        search: {
+            provider: 'local'  
+        }
+    }
 })
